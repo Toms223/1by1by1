7ff5ffde8477e92e4b208b48a6c2f832111730e2
toms223 <tomamiramart@gmail.com>
2023-01-19 14:10:22 +0000

    Finished basic data and api for the minecraft plugin functions.

--- data/111.minecraft.api.mjs
+++ data/111.minecraft.api.mjs
@@ -10,8 +10,24 @@ async function createInstance(username){
         method: "POST",
         headers: headers,
         body: JSON.stringify({username: username})
+    }).json()
+    return response.id
+}
+
+async function checkInstance(id){
+    let response = await fetch(`${APIUrl}/${id}`, {
+        method: "GET",
+        headers: headers
+    }).json()
+    return new Promise(async resolve => {
+        while(response.stopped !== true){
+            response = await fetch(`${APIUrl}/${id}`, {
+                method: "GET",
+                headers: headers
+            }).json()
+        }
+        resolve(response)
     })
-    return response.status
 }
 
 async function closeInstance(username, score){
